35aff0d821b9fc20a62573bd30963f56f248092f
ladanuzhna <54150144+ladanuzhna@users.noreply.github.com>
2020-06-20 07:13:53 -0500

    fixed reference search

--- src/load.py
+++ src/load.py
@@ -1,15 +1,18 @@
 import torch
 from src import model as m
 import wikipedia
-from lxml import html
-import requests
+import time
+from urllib.request import urlopen
 from googlesearch import search
+import en_core_web_sm
 
 # Generate masks for BERT model and prepare the text for training
 
 def ask_question_(question):
-    reference = find_reference(question)
-    print(reference)
+    print("Question:", question)
+    reference = find_references2(question)
+    if len(reference) == 0:
+        return
     model_,tokenizer_= m.get_model()
     encoded = tokenizer_.encode_plus(question, reference)
     # To separate our question and answer in the input to the model,
@@ -23,10 +26,10 @@ def ask_question_(question):
     [start,end] = model_(torch.tensor([encoded['input_ids']]),token_type_ids=torch.tensor([token_type_ids]))
     answer_start = torch.argmax(start)
     answer_end = torch.argmax(end)
+    answer__ = torch.max(start)
+    end__ = torch.max(end)
 
     tokens = tokenizer_.convert_ids_to_tokens(encoded['input_ids'])
-
-    print("Question:", question)
 
     answer = recreate_answer(tokens,answer_start, answer_end)
 
@@ -42,14 +45,63 @@ def recreate_answer(tokens, start, end):
     return answer
 
 def find_reference(question):
-    # goes to wikipedia for you, finds a reference passage
+    return wikipedia.summary(question, sentences=7)
 
-    # query = question + "wikipedia"
-    # link = next(search(query, tld='com', lang='en', num=1))
-    # page = requests.get(link)
-    # tree = html.fromstring(page.content)
+def find_references2(question):
+    entities = extract_entities(question)
+    summaries = []
+    for entity in entities:
+        try:
+            summaries.append(wikipedia.summary(entity, sentences=10))
+        except:
+            continue
+    if len(summaries) == 0:
+        try:
+            return wikipedia.summary(question, sentences=10)
+        except:
+            print("Sorry, the information was not found")
+    return summaries
 
-    return wikipedia.summary(question
-                             p, sentences=5)
+
+def extract_entities(question):
+    wanted = ['ORG','PERSON','GPE','LOC','WORK_OF_ART','LAW','NORP','FAC','PRODUCT','EVENT']
+    nlp = en_core_web_sm.load()
+    doc = nlp(question)
+    ents = [X.text for X in doc.ents if X.label_ in wanted ]
+    return ents
+
+
+def find_references(question, NUM_OF_REF = 3):
+    # Finds a specified number of references for the question asked
+    references = []
+    generator = search(question, tld='com', lang='en', num=NUM_OF_REF)
+    for link in generator:
+        text = extract_passage(link)
+        references.append(text)
+    references.append(wikipedia.summary(question, sentences=5))
+    return references
+
+def extract_passage(url):
+    try:
+        html = urlopen(url).read()
+        time.sleep(20)
+    except:
+        print("Waiting didn't really help")
+    soup = wikipedia.BeautifulSoup(html)
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()  # rip it out
+
+    # get text
+    text = soup.body.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
 
 
